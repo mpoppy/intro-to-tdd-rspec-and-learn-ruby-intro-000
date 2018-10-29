7b451d9f19e5b29ec05f10bbee447d691bc32868
Marc Poppy <poppy.marc@gmail.com>
2018-10-29 04:01:25 +0000

    automatically backed up by learn

---
+++ how_old_are_you.rb
@@ -0,0 +1,10 @@
+#new method to see how old are you
+
+puts "What year were you born"
+birth_year = gets. to_i
+
+users_age = current_age_for_birth_year (year)
+
+puts "You are: #{users_age.to_s} years old"
+
+
